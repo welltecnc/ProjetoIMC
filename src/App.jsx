33ce53bc58c17e501a5277f213f6a38917d5f67b
Wellington <welltecnic@hotmail.com>
2025-08-21 09:43:37 -0300

    criando a estutura código do forms no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-
+import Header from "./components/Header"
 
 function App() {
  
 
   return (
     // fragment
-    <>
-     
-    </>
+    <div className="container">
+      <div className="box">
+     <Header/>
+     <form>
+      <div>
+        <label htmlFor="altura"><span className="span">(exemplo: 1.80)</span></label>
+        <input type="number" id="altura" placeholder="Digite sua Altura"
+        onBlur={({target})=>setAltura(parseFloat(target.value))}/>
+      </div>
+
+      <div>
+        <label htmlFor="peso"><span className="span">(exemplo: 80)</span></label>
+        <input type="number" id="peso" placeholder="Digite seu Peso"
+        onBlur={({target})=>setPeso(parseFloat(target.value))}/>
+      </div>
+
+      <button onClick={calcularImc}>Calcular</button>
+     </form>
+    </div>
+    {/* MOSTRA O RESULTADO AO DIGITAR A ALTURA E O PESO */}
+    {mostrarResultado && (
+
+      <Resultado resultado={resultado}/>
+    )}
+    </div>
+
   )
 }
 
